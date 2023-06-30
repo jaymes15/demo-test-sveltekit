fbf7b6957f9c7f0222a9622ba9a7d3a6a22cf642
jaymes15 <jamesoluwatomisinomotosho@gmail.com>
2023-06-30 14:28:16 +0100

    first commit

--- src/components/About.test.js
+++ src/components/About.test.js
@@ -3,7 +3,7 @@ import About from './About.svelte';
 
 test('should render', () => {
 	const screen = render(About, { props: { label: 'a button' } });
-    screen.getByRole("heading", {name: "Sign Up"});
+    const header = screen.getByRole("heading", {name: "Sign Up"});
         
     expect(header).toBeInTheDocument();
 });
